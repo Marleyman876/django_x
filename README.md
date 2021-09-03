017ac15886406f00a5cfa48cb3088d7ac5b63ae3
Marleyman876 <grant_garfield@yahoo.com>
2021-09-02 19:49:44 -0700

    initial push

--- README.md
+++ README.md
@@ -14,6 +14,7 @@
 ----
 
 ## Table of Contents
+
 * **[Installation](#installation)**
   * [Pip](#pip)
   * [Pipenv](#pipenv)
---
+++ grocerylist_tracker/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GrocerylistTrackerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'grocerylist_tracker'
